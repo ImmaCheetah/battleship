a23ffd34e12456f84b0b2369353b8d1ca62bdcc1
Davit <29075792+ImmaCheetah@users.noreply.github.com>
2024-05-07 11:12:27 -0400

    test: add test for length and no argument in ship

--- src/modules/shipFactory.js
+++ src/modules/shipFactory.js
@@ -1,5 +1,9 @@
-function Ship() {
+function Ship(length) {
+    if (!length) throw ('Missing argument for ship');
 
+    
+
+    return { length }
 }
 
 export {Ship}
--- src/tests/shipFactory.test.js
+++ src/tests/shipFactory.test.js
@@ -3,3 +3,12 @@ import { Ship } from "../modules/shipFactory";
 test('Ship factory exists', () => {
     expect(Ship).toBeDefined();
 }) 
+
+test('Throws error if missing an argument', () => {
+    expect(() => Ship()).toThrow('Missing argument for ship');
+})
+
+test('Returns the length of ship', () => {
+    expect(Ship(4).length).toEqual(4);
+})
+
