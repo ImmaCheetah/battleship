dda119ffb5c57636d2533e73bacf0aba0428f091
Davit <29075792+ImmaCheetah@users.noreply.github.com>
2024-05-07 13:06:10 -0400

    test: add tests to check hit functions

--- src/modules/shipFactory.js
+++ src/modules/shipFactory.js
@@ -1,9 +1,16 @@
-function Ship(length) {
+function Ship(length, hits = 0) {
     if (!length) throw ('Missing argument for ship');
 
-    
+    const hit = () => {
+        hits += 1;
+    }
 
-    return { length }
+    return { length, 
+        hit, 
+        get hits() {
+            return hits;
+        }, 
+    }
 }
 
 export {Ship}
--- src/tests/shipFactory.test.js
+++ src/tests/shipFactory.test.js
@@ -12,3 +12,18 @@ test('Returns the length of ship', () => {
     expect(Ship(4).length).toEqual(4);
 })
 
+test('Hits parameter defaults as 0', () => {
+    let testShip = Ship(4);
+    expect(testShip.hits).toEqual(0);
+}) 
+
+test('Hit function exists', () => {
+    let testShip = Ship(2);
+    expect(testShip.hit).toBeDefined();
+})
+
+test('Hit function adds 1 to hits when called', () => {
+    let testShip = Ship(2);
+    testShip.hit()
+    expect(testShip.hits).toEqual(1);
+})
