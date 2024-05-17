df5b5e5babc7a6da3cb4e60c890d3d3f0fa03336
Davit <29075792+ImmaCheetah@users.noreply.github.com>
2024-05-17 14:04:54 -0400

    test: create test for player/computer boards

--- src/modules/playerFactory.js
+++ src/modules/playerFactory.js
@@ -1,5 +1,17 @@
-function Player() {
+import { Gameboard } from "./gameboardFactory"
 
+function Player(type) {
+    let playerBoard;
+    let computerBoard;
+    if (type === 'player') {
+        playerBoard = Gameboard();
+    } else if (type === 'computer') {
+        computerBoard = Gameboard();
+    } else {
+        throw ('Wrong input given');
+    }
+    
+    return {playerBoard, computerBoard}
 }
 
-export {Player}
+export { Player }
--- src/tests/playerFactory.test.js
+++ src/tests/playerFactory.test.js
@@ -1,5 +1,19 @@
+import { Gameboard } from "../modules/gameboardFactory";
 import { Player } from "../modules/playerFactory";
 
 test('Player factory exists', () => {
     expect(Player).toBeDefined();
+})
+
+test('Player returns correct board based on input', () => {
+    let testPlayer = Player('player');
+    let testComputer = Player('computer');
+    let testBoard = JSON.stringify(Gameboard());
+    expect(JSON.stringify(testPlayer.playerBoard)).toBe(testBoard);
+    expect(JSON.stringify(testComputer.computerBoard)).toBe(testBoard);
+})
+
+test.skip('Throws an error if wrong input given', () => {
+    let testPlayer = Player('kablam');
+    expect(() => Player('kablam')).toThrow('Wrong input given');
 })
